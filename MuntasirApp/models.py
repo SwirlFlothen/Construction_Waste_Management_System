262e8e02c3407944848ee2e1b7881c5d96f02537
Tahsinul Muntasir Moon <muntasirmarshmallow@gmail.com>
2024-11-11 20:57:17 +0600

    project table in models

--- MuntasirApp/models.py
+++ MuntasirApp/models.py
@@ -7,6 +7,19 @@ class users(models.Model):
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=10)
 
-
     def __str__(self):
         return self.user_name
+
+
+class project(models.Model):
+    project_id = models.IntegerField(blank=True, null=True)
+    user_id = models.ForeignKey(users,on_delete=models.CASCADE,blank=True,null=True)
+    project_name = models.CharField(max_length=200)
+    duration = models.DateField(auto_now_add=True,auto_now=False)
+    estimated_waste = models.FloatField(blank=True,null=True)
+
+    def __str__(self):
+        return self.project_name
+
+
+
--- MuntasirApp/views.py
+++ MuntasirApp/views.py
@@ -3,8 +3,15 @@ from .models import *
 # Create your views here.
 def Home(request):
     object_user = users.objects.all()
-    context1 = {'object_user': object_user}
+    object_project = project.objects.all()
+    context1 = {'object_user': object_user,
+                 'object_project': object_project
+    }
     return render(request,template_name='MuntasirApp\Home.html',context=context1)
+
+
+
+
 
 def Services(request):
     return render(request,template_name='MuntasirApp\Services.html')
