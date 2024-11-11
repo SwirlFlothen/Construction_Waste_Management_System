db6bec8f2a4569d2c6780ba893a10e7ca46fa46a
Tahsinul Muntasir Moon <muntasirmarshmallow@gmail.com>
2024-11-11 20:17:23 +0600

    Models work for CWMS

--- MuntasirApp/models.py
+++ MuntasirApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class users(models.Model):
+    user_id = models.IntegerField(blank=True,null=True)
+    user_name= models.CharField(max_length=200)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=10)
+
+
+    def __str__(self):
+        return self.user_name
--- MuntasirApp/views.py
+++ MuntasirApp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def Home(request):
-    return render(request,template_name='MuntasirApp\Home.html')
+    object_user = users.objects.all()
+    context1 = {'object_user': object_user}
+    return render(request,template_name='MuntasirApp\Home.html',context=context1)
 
 def Services(request):
     return render(request,template_name='MuntasirApp\Services.html')
